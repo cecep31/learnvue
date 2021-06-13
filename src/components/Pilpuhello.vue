9e0890b79448c7c70269520951f3c67a3e9231c7
cecep31 <cecepjanuardi31@gmail.com>
2021-06-13 21:03:22 +0700

    reactive ngak bisa tiba2 bisa ngak jelas

---
+++ src/components/Pilpuhello.vue
@@ -0,0 +1,31 @@
+<template>
+    <input type="text" v-model="nama">
+    {{no}}
+    <button @click="tambah">tamabah nomor</button>
+    
+  <div>{{ nama }}</div>
+</template>
+
+<script>
+export default {
+    name: "Pilputhello",
+    data(){
+        return({
+            no: 0,
+            nama:'asad'
+        })
+    },
+    methods: {
+        tambah(){
+            this.no++
+        }
+    },
+    mounted(){
+        this.no++
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Pilput.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <pilputhello />
+  </div>
+</template>
+
+<script>
+import Pilputhello from '@/components/Pilpuhello.vue'
+
+export default {
+    name: 'Pilput',
+    components:{
+        Pilputhello
+    }
+}
+</script>
+
+<style>
+
+</style>
