9d77cc68aab7fa2943dd520646952ab29e950678
cecep31 <cecepjanuardi31@gmail.com>
2021-06-18 16:21:29 +0700

    animate work witout tranform

--- src/components/Pilpuhello.vue
+++ src/components/Pilpuhello.vue
@@ -1,10 +1,11 @@
 <template>
     <input type="text" v-model="nama">
     <div v-for="user in user" :key="user.id">{{user.nama}}</div>
-    <img alt="Vue logo" class="transform transition duration-150 hover:rotate-90 cursor-pointer opacity-20 hover:opacity-100  hover:translate-x-32" src="../assets/logo.png">
-    <div class="flex-container">
+    <img alt="Vue logo" class="transform transition duration-150 cursor-pointer" src="../assets/logo.png">
+    <img src="../assets/sdaaa.png" class="spin-slow mb-36 animate-spin mx-auto cursor-pointer w-12" alt="">
+    <div class="flex-container mt-36">
         <div class="nomor">{{no}}</div>
-        <button @click="tambah">tamabah nomor</button>
+        <button @click="tambah" class="text-white font-bold p-2 bg-purple-600 rounded-sm shadow-lg">tamabah nomor</button>
     </div>
     
 
@@ -30,7 +31,7 @@ export default {
     },
     methods: {
         tambah(){
-            this.no++
+            this.no = this.no+222222
         }
     },
     mounted(){
@@ -39,7 +40,7 @@ export default {
             this.user = res.data
         })
         setInterval(()=>{
-            this.no++
+            this.no = this.no+5
         },200)
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: { content: ['./public/**/*.html', './src/**/*.vue'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      Animation:{
+        'spin-slow':'spin 5s linear infinite',
+      }
+    },
   },
   variants: {
     extend: {},
